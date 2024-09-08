f53480e41c93f822fe414961947eb302fa9e279d
Alexis D. <dnc.alexis@gmail.com>
2024-09-08 16:14:54 +0200

    Update main.yml to o2switch deploy

--- server/certbot/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! -f /etc/letsencrypt/live/yourdomain.com/fullchain.pem ]; then
-  certbot certonly --webroot --webroot-path=/var/www/certbot \
-  --email youremail@domain.com --agree-tos --no-eff-email \
-  -d yourdomain.com
-fi
-
-exec "$@"
