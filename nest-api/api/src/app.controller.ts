91aac7b62774a05397dfca1dc9165f8be38b08cb
Alexis <dnc.alexis@gmail.com>
2024-10-27 19:14:02 +0100

    Refactor: redirect to hello world message if path is "/"

--- nest-api/api/src/app.controller.ts
+++ nest-api/api/src/app.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from './utils';
+import { Response } from 'express';
 
-@Controller('/api/v1/')
+@Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Public()
   @Get()
+  getHome(@Res() response: Response) {
+    return response.redirect('/api/v1/');
+  }
+
+  @Public()
+  @Get('/api/v1/')
   getHello(): string {
     return this.appService.getHello();
   }
