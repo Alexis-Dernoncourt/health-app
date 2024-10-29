3b9725c5f234d8a9fc0b0c7de796c6ff63860b7e
Alexis <dnc.alexis@gmail.com>
2024-10-29 19:24:32 +0100

    Add swagger documentation to main page instead of redirect

--- nest-api/api/src/app.controller.ts
+++ nest-api/api/src/app.controller.ts
@@ -1,17 +1,10 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from './utils';
-import { Response } from 'express';
 
 @Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Public()
-  @Get()
-  getHome(@Res() response: Response) {
-    return response.redirect('/api/v1/');
-  }
 
   @Public()
   @Get('/api/v1/')
--- nest-api/api/src/main.ts
+++ nest-api/api/src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ZodExceptionFilter } from './customExceptions/ZodErrorHandler';
 // import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new ZodExceptionFilter());
+  const config = new DocumentBuilder()
+    .setTitle('HEALTH-APP Api Documentation')
+    .setDescription('The api Swagger endpoints documentation')
+    .setVersion('1.0')
+    // .addTag('health-app')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, documentFactory);
   await app.listen(3333);
 }
 bootstrap();
